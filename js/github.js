679d81e37f18023bc08c8432da1d716e45c47dea
Kevin Johnston <kjohnston1149@gmail.com>
2016-10-17 16:04:34 -0700

    Pull repo name from API response

--- js/github.js
+++ js/github.js
@@ -5,8 +5,14 @@ function Github(){
 }
 
 Github.prototype.getRepos = function(){
+  $.get('https://api.github.com/users/kjohnston1149/repos?access_token=' + apiKey).then(function(public){
+    // console.log(JSON.stringify(public));
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
   $.get('https://api.github.com/users/kjohnston1149/repos?access_token=' + apiKey).then(function(response){
     console.log(JSON.stringify(response));
+    console.log(response[0].name)
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
