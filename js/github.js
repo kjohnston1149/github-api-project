35f2fbbbfcf7929af8535d991a5ec86fbb9004f2
Kevin Johnston <kjohnston1149@gmail.com>
2016-10-17 19:22:31 -0700

    Add browser display of a single repo name

--- js/github.js
+++ js/github.js
@@ -5,17 +5,14 @@ function Github(){
 }
 
 Github.prototype.getRepos = function(userName){
-  // $.get('https://api.github.com/users/'+ userName + '/repos?access_token=' + apiKey).then(function(public){
-  //   // console.log(JSON.stringify(public));
-  // }).fail(function(error){
-  //   console.log(error.responseJSON.message);
-  // });
+
   $.get('https://api.github.com/users/'+ userName + '/repos?access_token=' + apiKey).then(function(response){
     console.log(JSON.stringify(response));
     repoName = response[0].name;
     repoDescription = response[0].description;
-    console.log(repoName);
-    console.log(repoDescription);
+    console.log(response[0].description);
+    $('#nameOfRepo').text(repoName);
+    $('#descriptionOfRepo').text(descriptionOfRepo);
     return repoName;
     return repoDescription;
   }).fail(function(error){
